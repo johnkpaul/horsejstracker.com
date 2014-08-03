7881663580d00c6b4c6575ce0854c385c9e9846a
John K. Paul <john@johnkpaul.com>
2014-08-03 17:55:28 -0400

    Add mapping for https://twitter.com/horse_js/statuses/495997098853421056

--- server/lib/mapping.js
+++ server/lib/mapping.js
@@ -3,7 +3,8 @@ var mapping = {
   'https://twitter.com/horse_js/status/495646693359374336': 'https://twitter.com/searls/status/495405355514744832',
   'https://twitter.com/horse_js/status/495316559909224448': 'https://twitter.com/wycats/status/495315214418468866',
   'https://twitter.com/horse_js/status/495635328703934464': 'https://twitter.com/potch/status/361966029234122754',
-  'https://twitter.com/horse_js/status/495365980760322048': 'https://twitter.com/swannodette/status/495364324693913600'
+  'https://twitter.com/horse_js/status/495365980760322048': 'https://twitter.com/swannodette/status/495364324693913600',
+  "https://twitter.com/horse_js/statuses/495997098853421056": "https://twitter.com/monsika/status/495994787737395200"
 };
 
 var _ = require('lodash');
