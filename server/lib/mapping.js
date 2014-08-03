80d977f0b799aa1dfa25ee71e3181e579e3be92a
Brendan McLoughlin <btmcloughlin@gmail.com>
2014-08-03 12:03:59 -0400

    Add mapping for "how much I enjoy codin' JS"

--- server/lib/mapping.js
+++ server/lib/mapping.js
@@ -1,7 +1,9 @@
 var mapping = {
   'https://twitter.com/horse_js/statuses/495576140334243840': 'https://twitter.com/fivetanley/status/495404763081478144',
   'https://twitter.com/horse_js/status/495646693359374336': 'https://twitter.com/searls/status/495405355514744832',
-  'https://twitter.com/horse_js/status/495316559909224448': 'https://twitter.com/wycats/status/495315214418468866'
+  'https://twitter.com/horse_js/status/495316559909224448': 'https://twitter.com/wycats/status/495315214418468866',
+  'https://twitter.com/horse_js/status/495365980760322048': 'https://twitter.com/swannodette/status/495364324693913600'
+
 };
 
 
