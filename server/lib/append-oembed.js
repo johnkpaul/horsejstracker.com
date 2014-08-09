4edea2f2bc81491366ac01dd82053d37c5c6ba4e
John K. Paul <john@johnkpaul.com>
2014-08-09 11:36:35 -0400

    Add memoization of oembed calls to twitter

--- server/lib/append-oembed.js
+++ server/lib/append-oembed.js
@@ -4,7 +4,23 @@ var q = require('q');
 var _ = require('lodash');
 var util = require('util');
 
+var getOembed = function(tweet_id){
+  var def = q.defer(); 
+  if(tweet_id){
+    twit.get('/statuses/oembed.json', {id: tweet_id}, function(data) {
+      def.resolve(data);
+    });
+  }
+  else{
+    def.resolve({});
+  }
+  return def.promise;
+}
+
+getOembed = _.memoize(getOembed);
+
 module.exports = function(tweets){
+
   var oEmbedRequests = tweets.map(function(tweet){
      var inspiration = mapping[_.last(tweet.id_str.split('/'))];
      if(inspiration){
@@ -23,15 +39,3 @@ module.exports = function(tweets){
   return q.all(oEmbedRequests);
 }
 
-function getOembed(tweet_id){
-  var def = q.defer(); 
-  if(tweet_id){
-    twit.get('/statuses/oembed.json', {id: tweet_id}, function(data) {
-      def.resolve(data);
-    });
-  }
-  else{
-    def.resolve({});
-  }
-  return def.promise;
-}
