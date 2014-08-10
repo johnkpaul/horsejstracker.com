8664a1214e4e990882ec67a6d74857f27a85d4d2
John K. Paul <john@johnkpaul.com>
2014-08-09 22:39:13 -0400

    Add progressive saving from twitter api to load script

--- server/db/loadTweets.js
+++ server/db/loadTweets.js
@@ -1,16 +1,27 @@
 var env = process.env.NODE_ENV;
 var knex = require('knex')(require('./knexfile')[env]);
+var Q = require('q');
 
 var twitGet = require('../lib/twitGet');
-twitGet('/statuses/user_timeline.json', {screen_name: 'horse_js', count: 40, include_rts: 0})
-         .then(function(data) {
-            return data.map(function(tweet){
-              return knex('tweets').insert({
-                horse_js_tweet_id_str: tweet.id_str,
-                horse_js_tweet_data: tweet
-              });
-            });
-          }).done(null, function(err){
-            console.log("twitter get failed", err);
-          });
+knex.select('*').from('tweets').orderBy('horse_js_tweet_id_str', 'desc').limit(1).then(function(rows){
+  if (rows.length === 0){
+    return '491729185514061824';
+  } 
+  else{
+    return rows[0].horse_js_tweet_data.id;
+  }
+}).then(function(since_id){
+
+  return twitGet('/statuses/user_timeline.json', {screen_name: 'horse_js', count: 10, include_rts: 0, since_id: since_id})
+
+}).then(function(tweets) { 
+  return Q.all(tweets.map(function(tweet){
+    return knex('tweets').insert({
+      horse_js_tweet_id_str: tweet.id_str,
+      horse_js_tweet_data: tweet
+    });
+  }));
+}).done(null, function(err){
+  console.log("twitter get failed", err);
+});
 
